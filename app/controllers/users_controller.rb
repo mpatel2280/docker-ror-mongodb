2945f43ad4f70d004a5d1e8d993aa85bbdadbe24
Manish Patel <patelm.2280@gmail.com>
2024-11-19 10:34:45 +0530

    dockerise ror and mongodb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+    
+    before_action :find_user_by_id, except: %i[create new index]
+
+    def index
+      @users = User.all
+      render json: @users
+    end
+  
+    def show
+      @user = User.find(params[:id])
+      render json: @user
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to @user, notice: 'User was successfully created.'
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      @user = User.find(params[:id])
+    end
+  
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to @user, notice: 'User was successfully updated.'
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @user = User.find(params[:id])
+      @user.destroy
+      redirect_to users_url, notice: 'User was successfully destroyed.'
+    end
+  
+    private
+  
+    def find_user_by_id
+        @user = User.find_by(id: params[:id])
+        if @user
+            render json: @user
+        else
+            render json: { error: 'User not found' }, status: :not_found
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  end
+  
---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  resources :posts
+
+  resources :users
+
+  resources :books
+
+  resources :products
+
+  get '/get_products', to: "products#get_products"
+
+  # get 'list_files', to: 'list_directory_files#index'
+  # #root 'list_directory_files#index'
+
+  # get 'export_data/export', to: 'export_data#export'
+
+end
